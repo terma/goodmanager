cc8006cb701d30cbeb7ef78399df21971c8b8557
artem.stasuk@gmail.com <artem.stasuk@gmail.com@972612ee-7b2c-9843-d6cf-930f95fdaa46>
2011-11-12 18:46:16 +0000

    reformat.

--- src/ua/com/testes/manager/logic/mail/MailProviderExchange.java
+++ src/ua/com/testes/manager/logic/mail/MailProviderExchange.java
@@ -1,28 +1,23 @@
 package ua.com.testes.manager.logic.mail;
 
-import com.google.inject.ImplementedBy;
 import ua.com.testes.manager.logic.mail.content.MailContent;
 
 import java.util.List;
 
-@ImplementedBy(MailProviderExchangeImap4.class)
-public abstract interface MailProviderExchange extends MailProvider {
-    public abstract List<MailFolder> getRootFolders();
+public interface MailProviderExchange extends MailProvider {
 
-    public abstract List<MailFolder> getFolders(String paramString);
+    List<MailFolder> getRootFolders();
 
-    public abstract List<MailItem> getMessages(String paramString);
+    List<MailFolder> getFolders(String paramString);
 
-    public abstract MailItem getMessage(String paramString, int paramInt);
+    List<MailItem> getMessages(String paramString);
 
-    public abstract List<MailContent> getContents(String paramString, int paramInt);
+    MailItem getMessage(String paramString, int paramInt);
 
-    public abstract void setMessageReaded(String paramString, int paramInt);
+    List<MailContent> getContents(String paramString, int paramInt);
 
-    public abstract void deleteMessage(String paramString, int paramInt);
+    void setMessageReaded(String paramString, int paramInt);
+
+    void deleteMessage(String paramString, int paramInt);
+
 }
-
-/* Location:           C:\artem\work\goodmanager\web\WEB-INF\classes\
- * Qualified Name:     ua.com.testes.manager.logic.mail.MailProviderExchange
- * JD-Core Version:    0.6.0
- */
