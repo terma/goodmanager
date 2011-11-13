785a693297c68ef4ef97ad9f5c285ac87432bdb4
artem.stasuk@gmail.com <artem.stasuk@gmail.com@972612ee-7b2c-9843-d6cf-930f95fdaa46>
2011-11-13 00:14:43 +0000

    1. Move entities declaration to persistence.xml 2. Add all jar like maven dependency 3. Fix incorrect links over tag login

--- src/ua/com/testes/manager/entity/EntityManager.java
+++ src/ua/com/testes/manager/entity/EntityManager.java
@@ -1,31 +1,8 @@
 package ua.com.testes.manager.entity;
 
-import org.hibernate.ejb.Ejb3Configuration;
-import ua.com.testes.manager.entity.content.EntityImage;
-import ua.com.testes.manager.entity.content.EntityText;
-import ua.com.testes.manager.entity.mail.EntityMail;
-import ua.com.testes.manager.entity.mail.EntityPart;
-import ua.com.testes.manager.entity.mail.EntityPartFile;
-import ua.com.testes.manager.entity.mail.EntityPartText;
-import ua.com.testes.manager.entity.mail.server.EntityServer;
-import ua.com.testes.manager.entity.mail.server.rule.EntityServerRule;
-import ua.com.testes.manager.entity.mail.server.rule.EntityServerRuleAll;
-import ua.com.testes.manager.entity.product.EntityCategory;
-import ua.com.testes.manager.entity.product.EntityCurrency;
-import ua.com.testes.manager.entity.product.EntityProduct;
-import ua.com.testes.manager.entity.product.EntityProductPrice;
-import ua.com.testes.manager.entity.search.*;
-import ua.com.testes.manager.entity.task.EntityTask;
-import ua.com.testes.manager.entity.task.EntityTaskExecutor;
-import ua.com.testes.manager.entity.tiding.EntityTiding;
-import ua.com.testes.manager.entity.tiding.EntityTidingCategory;
-import ua.com.testes.manager.entity.user.EntityUser;
-import ua.com.testes.manager.entity.view.EntityFirmSort;
-import ua.com.testes.manager.entity.view.EntityView;
-import ua.com.testes.manager.entity.view.EntityViewUser;
-
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.FlushModeType;
+import javax.persistence.Persistence;
 import javax.persistence.Query;
 import java.util.List;
 import java.util.Properties;
@@ -36,6 +13,7 @@ public class EntityManager {
             = new ThreadLocal<javax.persistence.EntityManager>();
     private static EntityManagerFactory factory = null;
 
+    @SuppressWarnings({"deprecation"})
     public static void configuration(String login, String password, String driver, String url, String dialect) {
         final Properties properties = new Properties();
         properties.setProperty("hibernate.dialect", dialect);
@@ -50,169 +28,27 @@ public class EntityManager {
         properties.setProperty("hibernate.generate_statistics", "false");
         properties.setProperty("hibernate.bytecode.use_reflection_optimizer", "false");
         properties.setProperty("hibernate.cglib.use_reflection_optimizer", "false");
-        properties.setProperty("hibernate.cache.provider_class", "org.hibernate.cache.EhCacheProvider");
-        properties.setProperty("hibernate.hbm2ddl.auto", "update");
+        properties.setProperty("hibernate.cache.provider_class", "net.sf.ehcache.hibernate.EhCacheProvider");
+//        properties.setProperty("hibernate.hbm2ddl.auto", "update");
 
-        final Ejb3Configuration configuration = new Ejb3Configuration().addProperties(properties);
-        configuration.addPackage(EntityManager.class.getPackage().getName());
-        configuration.addAnnotatedClass(EntityImage.class);
-        configuration.addAnnotatedClass(EntityFirm.class);
-        configuration.addAnnotatedClass(EntitySection.class);
-        configuration.addAnnotatedClass(EntityRank.class);
-        configuration.addAnnotatedClass(EntityUser.class);
-        configuration.addAnnotatedClass(EntityPipol.class);
-        configuration.addAnnotatedClass(EntityStatus.class);
-        configuration.addAnnotatedClass(EntityContact.class);
-        configuration.addAnnotatedClass(EntityContactHistory.class);
-        configuration.addAnnotatedClass(EntityGroup.class);
-        configuration.addAnnotatedClass(EntityRule.class);
-        configuration.addAnnotatedClass(EntityStyle.class);
-        configuration.addAnnotatedClass(EntityFirmHistory.class);
-        configuration.addAnnotatedClass(EntityPipolHistory.class);
-        configuration.addAnnotatedClass(EntityContract.class);
-        configuration.addAnnotatedClass(EntityContractVersion.class);
-        configuration.addAnnotatedClass(EntityVersionFile.class);
-        configuration.addAnnotatedClass(EntityVersionResolution.class);
-        configuration.addAnnotatedClass(EntityTask.class);
-        configuration.addAnnotatedClass(EntityView.class);
-        configuration.addAnnotatedClass(EntityFirmSort.class);
-        configuration.addAnnotatedClass(EntityViewUser.class);
-        configuration.addAnnotatedClass(EntityText.class);
-        configuration.addAnnotatedClass(EntityProduct.class);
-        configuration.addAnnotatedClass(EntityProductPrice.class);
-/*  87 */
-        configuration.addAnnotatedClass(EntityCurrency.class);
-/*  88 */
-        configuration.addAnnotatedClass(EntityCategory.class);
-/*  89 */
-        configuration.addAnnotatedClass(EntityTidingCategory.class);
-/*  90 */
-        configuration.addAnnotatedClass(EntityTiding.class);
-/*  91 */
-        configuration.addAnnotatedClass(EntitySearch.class);
-/*  92 */
-        configuration.addAnnotatedClass(EntitySearchRule.class);
-/*  93 */
-        configuration.addAnnotatedClass(EntitySearchSource.class);
-
-/*  95 */
-        configuration.addAnnotatedClass(EntitySearchRuleFirmSection.class);
-/*  96 */
-        configuration.addAnnotatedClass(EntitySearchRuleFirmSectionItem.class);
-/*  97 */
-        configuration.addAnnotatedClass(EntitySearchRuleFirmUser.class);
-/*  98 */
-        configuration.addAnnotatedClass(EntitySearchRuleFirmUserItem.class);
-/*  99 */
-        configuration.addAnnotatedClass(EntitySearchRuleFirmName.class);
-
-        configuration.addAnnotatedClass(EntitySearchSourceFirm.class);
-
-        configuration.addAnnotatedClass(EntitySearchRuleFirmOther.class);
-
-        configuration.addAnnotatedClass(EntitySearchRuleFirmCreate.class);
-
-        configuration.addAnnotatedClass(EntitySearchRuleFirmDelete.class);
-
-        configuration.addAnnotatedClass(EntitySearchRuleFirmComposite.class);
-
-        configuration.addAnnotatedClass(EntitySearchRuleFirm.class);
-
-
-        configuration.addAnnotatedClass(EntitySearchRulePipolComposite.class);
-
-        configuration.addAnnotatedClass(EntitySearchSourcePipol.class);
-
-        configuration.addAnnotatedClass(EntitySearchRulePipolFio.class);
-
-        configuration.addAnnotatedClass(EntitySearchRulePipolOther.class);
-
-        configuration.addAnnotatedClass(EntitySearchRulePipolDelete.class);
-
-        configuration.addAnnotatedClass(EntitySearchRulePipolCreate.class);
-
-        configuration.addAnnotatedClass(EntitySearchRulePipolUser.class);
-
-        configuration.addAnnotatedClass(EntitySearchRulePipolUserItem.class);
-
-        configuration.addAnnotatedClass(EntitySearchRulePipol.class);
-
-
-        configuration.addAnnotatedClass(EntitySearchRuleContactComposite.class);
-
-        configuration.addAnnotatedClass(EntitySearchSourceContact.class);
-
-        configuration.addAnnotatedClass(EntitySearchRuleContactDescription.class);
-
-        configuration.addAnnotatedClass(EntitySearchRuleContactCreate.class);
-
-        configuration.addAnnotatedClass(EntitySearchRuleContactRepeat.class);
-
-        configuration.addAnnotatedClass(EntitySearchRuleContactDelete.class);
-
-        configuration.addAnnotatedClass(EntitySearchRuleContactUser.class);
-
-        configuration.addAnnotatedClass(EntitySearchRuleContactUserItem.class);
-
-        configuration.addAnnotatedClass(EntitySearchRuleContactStatus.class);
-
-        configuration.addAnnotatedClass(EntitySearchRuleContactStatusItem.class);
-
-        configuration.addAnnotatedClass(EntitySearchRuleContact.class);
-
-
-        configuration.addAnnotatedClass(EntityMail.class);
-
-        configuration.addAnnotatedClass(EntityPart.class);
-
-        configuration.addAnnotatedClass(EntityPartFile.class);
-
-        configuration.addAnnotatedClass(EntityPartText.class);
-
-        configuration.addAnnotatedClass(EntityServer.class);
-
-        configuration.addAnnotatedClass(EntityServerRule.class);
-
-        configuration.addAnnotatedClass(EntityServerRuleAll.class);
-
-
-        configuration.addAnnotatedClass(EntityTask.class);
-
-        configuration.addAnnotatedClass(EntityTaskExecutor.class);
-
-        configuration.buildMappings();
-        try {
-            //noinspection deprecation
-            factory = configuration.createEntityManagerFactory();
-        } catch (Throwable exception) {
-            System.err.println("Initial SessionFactory creation failed." + exception);
-            throw new ExceptionInInitializerError(exception);
-        }
+        factory = Persistence.createEntityManagerFactory("persistence-unit", properties);
     }
 
     @SuppressWarnings({"unchecked"})
     public static <T> List<T> list(String hql, Object... parameters) {
-
         Query query = managers.get().createQuery(hql);
-
         for (int i = 0; i < parameters.length; i++) {
-
             query.setParameter("p" + i, parameters[i]);
         }
-
         return query.getResultList();
     }
 
     @SuppressWarnings({"unchecked"})
     public static <T> List<T> listByName(String hqlQueryName, Object[] parameters) {
-
         Query query = managers.get().createNamedQuery(hqlQueryName);
-
         for (int i = 0; i < parameters.length; i++) {
-
             query.setParameter("p" + i, parameters[i]);
         }
-
         return query.getResultList();
     }
 
@@ -226,14 +62,10 @@ public class EntityManager {
 
     @SuppressWarnings({"unchecked"})
     public static <T> T one(String hql, Object[] parameters) {
-
         Query query = managers.get().createQuery(hql);
-
         for (int i = 0; i < parameters.length; i++) {
-
             query.setParameter("p" + i, parameters[i]);
         }
-
         return (T) query.getSingleResult();
     }
 
