730a07e049b45ca576b93650c26b5836cbb83e55
artem.stasuk@gmail.com <artem.stasuk@gmail.com@972612ee-7b2c-9843-d6cf-930f95fdaa46>
2011-11-13 00:14:43 +0000

    1. Move entities declaration to persistence.xml 2. Add all jar like maven dependency 3. Fix incorrect links over tag login

--- trunk/src/ua/com/testes/manager/web/tag/TagLoginLinkGet.java
+++ trunk/src/ua/com/testes/manager/web/tag/TagLoginLinkGet.java
@@ -6,74 +6,47 @@ import java.io.IOException;
 
 
 public class TagLoginLinkGet extends TagSupport {
+
     private String always;
     private String value;
 
-
     private void write(String link) {
-
         try {
-
             this.pageContext.getOut().print(link);
-
         } catch (IOException exception) {
-
             throw new IllegalStateException(exception);
-
         }
-
     }
-
 
     private boolean isUseAlways() {
-
         return "true".equalsIgnoreCase(this.always);
-
     }
 
-
+    @Override
     public final int doStartTag() {
-//     if (isUseAlways())
+        write(value);
+//        if (isUseAlways()) {
 //       write(UtilLoginNtlm.convert(this.value));
-//     else {
+//        } else {
 //       write(UtilLoginNtlm.convertIsForRequest(this.value, (HttpServletRequest)this.pageContext.getRequest()));
-//     }
-
+//        }
         return 1;
-
     }
-
 
     public String getAlways() {
-
         return this.always;
-
     }
-
 
     public void setAlways(String always) {
-
         this.always = always;
-
     }
-
 
     public String getValue() {
-
         return this.value;
-
     }
 
-
     public void setValue(String value) {
-
         this.value = value;
-
     }
 
 }
-
-/* Location:           C:\artem\work\goodmanager\web\WEB-INF\classes\
- * Qualified Name:     ua.com.testes.manager.web.tag.TagLoginLinkGet
- * JD-Core Version:    0.6.0
- */
