a92bc1b19a3982ac8f9f926a581761e562c1b2d6
artem.stasuk@gmail.com <artem.stasuk@gmail.com@972612ee-7b2c-9843-d6cf-930f95fdaa46>
2012-03-04 10:08:59 +0000

    1. Remove unused classes. 2. Fix devide by zero in get average method in counter.

--- src/ua/com/testes/manager/web/filter/performan/PerformanCounter.java
+++ src/ua/com/testes/manager/web/filter/performan/PerformanCounter.java
@@ -2,42 +2,25 @@ package ua.com.testes.manager.web.filter.performan;
 
 
 public final class PerformanCounter {
-    /*  5 */   private volatile long time = 0L;
-    /*  6 */   private volatile int count = 0;
 
+    private volatile long time = 0L;
+    private volatile int count = 0;
 
     public long getTime() {
-
-        return this.time;
-
+        return time;
     }
-
 
     public int getCount() {
-
-        return this.count;
-
+        return count;
     }
-
 
     public double getAverage() {
-
-        return this.time / this.count;
-
+        return count == 0 ? 0 : time / count;
     }
 
-
     public synchronized void increment(long delay) {
-
-        this.time += delay;
-
-        this.count += 1;
-
+        time += delay;
+        count += 1;
     }
 
 }
-
-/* Location:           C:\artem\work\goodmanager\web\WEB-INF\classes\
- * Qualified Name:     ua.com.testes.manager.web.filter.performan.PerformanCounter
- * JD-Core Version:    0.6.0
- */
